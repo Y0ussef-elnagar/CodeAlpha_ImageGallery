4def39332ab8cb17ef1454a0824254755ec633a3
youssef elnagar <alnjary626@gmail.com>
2026-05-24 05:19:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const imageContainer = document.querySelector(".images-container");
+const btn = document.querySelector("button");
+let imageNum = 6;
+
+btn.addEventListener("click", () => {
+    addImages();
+});
+
+function addImages() {
+    for (let index = 0; index < imageNum; index++) {
+        const img = document.createElement("img");
+        img.src = `https://picsum.photos/200/300?random=${Math.random()}`;
+
+        imageContainer.appendChild(img);
+    }
+}
